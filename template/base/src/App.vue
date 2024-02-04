c55760ba8eab0b1c7ec86a31bd34408d5fabe636
菲鸽 <1020103647@qq.com>
2024-02-04 15:33:48 +0800

    feat: delete template, i don't need it

--- template/base/src/App.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-import { onLaunch } from '@dcloudio/uni-app'
-
-onLaunch(() => {})
-</script>
-
-<style></style>
--- template/module/unocss/uno.config.js.data.mjs
+++
@@ -1,9 +0,0 @@
-export default function getData() {
-  return {
-    config: [{
-      id: 'unocss',
-      importer: `import { presetUni } from '@uni-helper/unocss-preset-uni'`,
-      initializer: 'presetUni()',
-    }],
-  }
-}
